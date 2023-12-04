16c9c1640ae56ab1b85764647cd90509cbec394a
Lê Tấn Lộc <20110379@student.hcmute.edu.vn>
2023-12-05 00:21:30 +0700

    ngay moi day nang luong

--- src/components/PostShare..tsx
+++ src/components/PostShare..tsx
@@ -6,202 +6,241 @@ import {
     CardBody,
     CardFooter,
     CardHeader,
-    Container,
+    Center,
     Flex,
     Heading,
     IconButton,
     Image,
     Input,
-
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuList,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
     Text,
     useDisclosure
 } from "@chakra-ui/react"
 import { FcPanorama } from "react-icons/fc";
-import { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from 'axios';
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { BiChat, BiLike, BiShare } from "react-icons/bi";
-import Header from "./Common/Header"
-import Sidebar from "./Sidebar";
-import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { EditIcon, DeleteIcon } from '@chakra-ui/icons'
 
 export function PostShare(data: any) {
-    const id = useParams();
-    // http://localhost:5000/api/v1/posts
-    console.log("---")
-    const token = localStorage.getItem("token");
-
-    const [main_post, setmain_post] = useState<any>();
-
-    const [comment, setcomment] = useState<any>([])
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                await axios.get(`http://127.0.0.1:5000/api/v1/posts/${id.id}`, {
-                    headers: {
-                        'Content-Type': 'multipart/form-data', 'authorization': `Bearer ${token}`,
-                    },
-                }).then(data => {
-                    if (data) {
-                        setmain_post(data.data["data"]["doc"])
-                        //   console.log("check_data",data.data.data["doc"])
-                    }
-                }).catch(err => {
-                    console.log(err)
-                })
-            }
-            catch {
-            }
-        };
-        fetchData();
-    }, []);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                await axios.get(`http://127.0.0.1:5000/api/v1/posts?replyTo=${id.id}`, {
-                    headers: {
-                        'Content-Type': 'multipart/form-data',
-                        'authorization': `Bearer ${token}`,
-                    },
-                }).then(data => {
-                    if (data) {
-                        console.log("check_data", data.data.data["doc"])
-                        setcomment(data.data.data["doc"])
-                    }
-                }).catch(err => {
-                    console.log(err)
-                })
-            }
-            catch {
-            }
-        };
-
-        fetchData();
-    }, []);
-
-    if (!main_post) {
-        return (<div></div>)
-    }
+    console.log(data)
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [previewImage, setPreviewImage] = useState<string | null>(null);
 
     var check_post = false
     try {
-        console.log("data", main_post.image[0]?.filename)
+        console.log("data", data.data.image[0].filename)
         check_post = true
     }
     catch {
 
     }
-    console.log("main_post")
 
-    console.log(comment)
+    var like_count = data.data.likes.length
+    if (like_count == 0) {
+        like_count = ""
+    }
 
 
-    return (
-        <>
-            <Header lastName={"Linh"} firstName={"Le"}
-                profilePic={""} />
-            <Flex letterSpacing={2}>
-                <Sidebar />
 
-                <Container alignContent={"center"}>
-                    <Card my={5}>
-                        <CardHeader >
-                            <Flex letterSpacing={4}>
-                                <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
-                                    <Avatar style={{ cursor: 'pointer' }} name='Segun Adebayo' src={`http://127.0.0.1:5000/uploads/${main_post.postedBy["profilePic"]?.filename}`} />
-                                    <Box>
-                                        <Heading size='sm'></Heading>
+    const [dataofreply, setdataofreply] = useState<any>(null);
+    const [isreply, setisreply] = useState(false);
 
-                                        <Text style={{ cursor: 'pointer' }}>{`${main_post.postedBy["firstName"]} ${main_post.postedBy["lastName"]}`}</Text>
-                                    </Box>
-                                </Flex >
-                                <IconButton
-                                    variant='ghost'
-                                    colorScheme='gray'
-                                    aria-label='See menu'
-                                    icon={<BsThreeDotsVertical />}
-                                />
-                            </Flex >
-                        </CardHeader >
-                        <CardBody >
-                            <Text>
-                                {main_post.content}
-                            </Text>
-                        </CardBody>
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                if (data.data.replyTo) {
+                    await axios.get(`http://127.0.0.1:5000/api/v1/posts/${data.data.replyTo}`).then(data => {
+                        console.log(data.data["data"]["doc"])
+                        setdataofreply(data.data["data"]["doc"])
+                        setisreply(true)
+                    })
+                }
+            }
+            catch {
+            }
+        };
 
-                        {check_post && main_post && main_post.image && main_post.image[0] && (
-                            <Image
-                                minW={"400px"}
-                                minH={"400px"}
-                                objectFit='cover'
-                                src={`http://127.0.0.1:5000/uploads/${main_post.image[0]?.filename}`}
-                            />
-                        )}
-                        <CardFooter
-                            justify='space-between'
-                            flexWrap='wrap'
-                            sx={{
-                                '& > button': {
-                                    minW: '136px',
-                                },
-                            }}
-                        >
-                            <Button id={main_post._id} flex='1' variant='ghost' leftIcon={<BiLike className='bi-like'
-                            />}
-                            >
-                                {`Like`}
-                            </Button>
+        fetchData();
+    }, []);
 
-                            <Button flex='1' variant='ghost' leftIcon={<BiChat />}  >
-                                Comment
-                            </Button>
 
-                            <Button flex='1' variant='ghost' leftIcon={<BiShare />}>
-                                Share
-                            </Button>
-                        </CardFooter>
+    const handlelike = (id: number) => {
+        console.log(id)
+        axios.put(`http://127.0.0.1:5000/api/v1/posts/${id}/like`);
+        const button = document.getElementById(
+            id.toString(),
+        ) as HTMLInputElement;
 
-                        <Card >
-                            <Flex ml={5} margin={5} letterSpacing={4} >
+        const icon = button?.querySelector('.bi-like') as HTMLElement;
+        const like_c = button.textContent
+        console.log(icon.style.color)
 
-                                <Avatar style={{ cursor: 'pointer' }} name='Segun Adebayo' src={`http://127.0.0.1:5000/uploads/${main_post.postedBy["profilePic"]?.filename}`} />
-                                <Input ml={5} minH={45} ></Input>
-                                <Button marginLeft={2} minH={45} minW={20} flex='1' variant='ghost' >
-                                    Post
-                                </Button>
-                            </Flex>
 
-                        </Card>
+        if (icon) {
+            if (icon.style.color === "black") {
+                icon.style.color = "blue";
+            } else {
 
-                        <Card>
+                icon.style.color = "black";
+            }
+        }
+    }
 
-                            {comment.map((commentItem: any, index: any) => (
-                                <Flex key={index} ml={5} margin={5} letterSpacing={4}>
-                                    <Avatar
-                                        style={{ cursor: 'pointer' }}
-                                        name='Segun Adebayo'
-                                        src={`http://127.0.0.1:5000/uploads/${commentItem.postedBy["profilePic"]?.filename}`}
-                                    />
 
-                                    <Flex direction="column" marginLeft={5}>
-                                        <Text fontSize={15} align="left" fontWeight="bold" style={{ cursor: 'pointer' }}>
-                                            {`${commentItem.postedBy["firstName"]} ${commentItem.postedBy["lastName"]}`}
-                                        </Text>
-                                        <Text fontSize={15} whiteSpace="pre-line" align={"left"}>
-                                            {commentItem.content}
-                                        </Text>
-                                    </Flex>
-                                </Flex>
-                            ))}
+    const [islike, setIslike] = useState('black');
 
-                        </Card>
-                    </Card >
-                </Container>
-            </Flex>
-        </>
 
-    );
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                await axios.get(`http://127.0.0.1:5000/api/v1/posts/${data.data._id}/like`).then(data => {
+                    if (data && data.data.data.isLiked) {
+                        setIslike('blue')
+                    }
+                })
+
+            }
+            catch {
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    // ------------
+
+
+    const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+    const handleClickSelectFile = () => {
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
+    };
+
+    const [formDataPost, setFormDataPost] = useState({
+        content: "", image: File || null,
+
+    });
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setFormDataPost((prevFormDataPost) => ({
+            ...prevFormDataPost, [name]: value,
+        }));
+    };
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedFile = event.target.files && event.target.files[0];
+        let { name, value } = event.target;
+        name = "image"
+
+        console.log("name", name)
+        console.log("File changed", value)
+        setFormDataPost((prevFormDataPost) => ({
+            ...prevFormDataPost, [name]: selectedFile,
+        }));
+
+    };
+
+    const handleSubmit2 = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        try {
+            console.log("onSubmit")
+            console.log(formDataPost);
+            console.log("----loading--")
+            const token = localStorage.getItem("token");
+
+            console.log("token", token)
+            console.log(`id is ${data.data._id}`)
+
+
+            axios.post('http://localhost:5000/api/v1/posts', {
+                content: formDataPost.content,
+                image: formDataPost.image,
+                replyTo: data.data._id,
+            }, {
+                headers: {
+                    'Content-Type': 'multipart/form-data', 'authorization': `Bearer ${token}`,
+                },
+            }).then(response => {
+                console.log(response.data);
+            }).catch(error => {
+                console.log(error);
+            });
+            // Reset the form
+            setFormDataPost({
+                content: "", image: File
+            });
+
+        } catch (e) {
+            console.log(e)
+        }
+
+    }
+
+    const navigate = useNavigate();
+
+    const handlenav = (id: string) => {
+        navigate("/post/" + id);
+    }
+
+
+    const profileclick = () => {
+        // direct to profile
+    }
+
+    const postclick = (id: string) => {
+        // handlenav(id)
+        handlenav(id)
+    }
+
+    // console.log(data.postedBy["profilePic"].filename)
+
+    return (<Card backgroundColor={"lightgray"} >
+
+
+        {
+            check_post ? (<Image
+                objectFit='cover'
+                src={`http://127.0.0.1:5000/uploads/${data.data.image[0].filename}`}
+            />) : null
+        }
+
+        <CardHeader >
+            <Flex letterSpacing={4}>
+                <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
+                    <Avatar style={{ cursor: 'pointer' }} name='Segun Adebayo' src={`http://127.0.0.1:5000/uploads/${data.data.postedBy["profilePic"].filename}`} />
+                    <Box>
+                        <Heading size='sm'></Heading>
+
+                        <Text style={{ cursor: 'pointer' }}>{`${data.data.postedBy["firstName"]} ${data.data.postedBy["lastName"]}`}</Text>
+                    </Box>
+                </Flex >
+
+            </Flex >
+        </CardHeader >
+
+
+
+        <CardBody onClick={() => postclick(data.data._id)} alignItems={"left"} style={{ cursor: 'pointer' }}>
+            <Text>
+                {data.data.content}
+            </Text>
+        </CardBody>
+
+    </Card >);
 }
